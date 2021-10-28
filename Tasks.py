0d0c8c836f19f9495cd66b962ffbd9a77a6bc2ca
GustavRathsach <93138527+GustavRathsach@users.noreply.github.com>
2021-10-28 12:08:39 +0200

    Update Tasks.py

--- Tasks.py
+++ Tasks.py
@@ -3,14 +3,17 @@
 
 #create dict for signal description file
 
+
 #function for giving a score og sammenligne med deviation
 
-#function that reads fasta file
+#program that reads fasta file
   #remember ID line
   #check if score is good enough, or pass to next entry
   #identify first 6 and last 6 bases and their locations
   #write it afterwards
   
+#for the program, the following function can be created:
+  #function der identificerer  baser og locations
  
 
 
